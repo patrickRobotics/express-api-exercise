6988ed4e0cd95751732871780fe501eba66889d3
patrickRobotics <pirungu9@gmail.com>
2019-10-02 00:12:04 +0300

    add body parsers for POSTs

--- recipe-backend/app.js
+++ recipe-backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+app.use(bodyParser.json());
 
 app.use((req, res) => {
     res.json({ message: 'Your request was successful!' }); 
